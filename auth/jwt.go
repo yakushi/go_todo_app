671643acd4227f6d2276ba46eb955df9f4b9081e
Y, Ryota <yakushi@isonly.net>
2024-10-20 03:10:41 +0000

    P. 234

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/yakushi/go_todo_app/clock"
 	"github.com/yakushi/go_todo_app/entity"
 )
@@ -50,4 +54,33 @@ func parse(rawKey []byte) (jwk.Key, error) {
 		return nil, err
 	}
 	return key, nil
+}
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
+)
+
+func (j *JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	tok, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer("github.com/yakushi/go_todo_app").
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).
+		Claim(UserNameKey, u.Name).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("GetToken: failes to build token: %w", err)
+	}
+	if err := j.Store.Save(ctx, tok.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	signed, err := jwt.Sign(tok, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
 }
--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -2,7 +2,11 @@ package auth
 
 import (
 	"bytes"
+	"context"
 	"testing"
+
+	"github.com/yakushi/go_todo_app/entity"
+	"github.com/yakushi/go_todo_app/fixture"
 )
 
 func TestEmbed(t *testing.T) {
@@ -13,5 +17,29 @@ func TestEmbed(t *testing.T) {
 	want = []byte("-----BEGIN PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPrivKey)
+	}
+}
+
+func TestJWTer_GenerateToken(t *testing.T) {
+	ctx := context.Background()
+	moq := &StoreMock{}
+	wantID := entity.UserID(20)
+	u := fixture.User(&entity.User{ID: wantID})
+	moq.SaveFunc = func(ctx context.Context, key string, userID entity.UserID) error {
+		if userID != wantID {
+			t.Errorf("want %d, but got %d", wantID, userID)
+		}
+		return nil
+	}
+	sut, err := NewJWTer(moq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := sut.GenerateToken(ctx, *u)
+	if err != nil {
+		t.Fatalf("not want err: %v", err)
+	}
+	if len(got) == 0 {
+		t.Errorf("token is empty")
 	}
 }
