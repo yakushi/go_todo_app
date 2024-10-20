821d20249cc98149d9528b9495ab0f0ee48d75b4
Y, Ryota <yakushi@isonly.net>
2024-10-20 02:41:24 +0000

    P. 231 go get github.com/lestrrat-go/jwx/v2/jwk に修正

---
+++ auth/jwt.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/yakushi/go_todo_app/clock"
+	"github.com/yakushi/go_todo_app/entity"
+)
+
+//go:embed cert/secret.pem
+var rawPrivKey []byte
+
+//go:embed cert/public.pem
+var rawPubKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store                 Store
+	Clocker               clock.Clocker
+}
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
+	Save(ctx context.Context, key string, userID entity.UserID) error
+	Load(ctx context.Context, key string) (entity.UserID, error)
+}
+
+func NewJWTer(s Store) (*JWTer, error) {
+	j := &JWTer{Store: s}
+	privkey, err := parse(rawPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: private key: %w", err)
+	}
+	pubkey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: public key: %w", err)
+	}
+	j.PrivateKey = privkey
+	j.PublicKey = pubkey
+	j.Clocker = clock.RealClocker{}
+	return j, nil
+}
+
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
